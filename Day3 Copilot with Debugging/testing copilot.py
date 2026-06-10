6644bc976240b1c6d18367278a4e76b5c323c85f
DeekshithaCyber <deekshitha4491@gmail.com>
2026-06-10 08:50:38 +0530

    Day 3 -Debugging with GitHub copilot

---
+++ Day3 Copilot with Debugging/testing copilot.py
@@ -0,0 +1,19 @@
+for i in range(10):
+    print(i)
+
+print("hello")
+
+if True :
+    print("this is true")
+
+
+a = 10 
+b= 10
+
+print(a + b)
+
+
+num1 = 10 
+num2 = 20 
+result = num1/num2
+print(result)
